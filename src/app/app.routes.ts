2a154649aa39a5d0f35fc1f64349cdb7f3e7d8d6
Radl Florian <florian.radl@hotmail.com>
2023-11-10 15:47:38 +0100

    Improvements for content layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
 import { PageModule } from './page/page.module';
 import { authenticationGuard } from './shared/authentication.guard';
+import { ContentlayoutComponent } from './core/contentlayout/contentlayout.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: 'pages',
+    component: ContentlayoutComponent,
     loadChildren: () => import('./page/page.module').then((m) => m.PageModule),
     canActivate: [authenticationGuard],
   },
-  { path: ' ', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
 ];
